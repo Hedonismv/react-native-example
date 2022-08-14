d591a01f039d110497cfbb8ca2375044d6fc9aac
Vitaly Klyhun <klygun90@gmail.com>
2022-08-14 14:56:18 +0300

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+## React Native App with Expo
---
+++ src/screens/about-screen.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {
+  ScrollView,
+  Box,
+  Text,
+  VStack,
+  Icon,
+  Image,
+  useColorModeValue
+} from 'native-base'
+import { Feather } from '@expo/vector-icons'
+import AnimatedColorBox from '../components/animated-color-box'
+import Navbar from '../components/navbar'
+import Masthead from '../components/masthead'
+import LinkButton from '../components/link-button'
+
+const AboutScreen = () => {
+  return (
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue('warmGray.50', 'warmGray.900')}
+      w="full"
+    >
+      <Masthead
+        title="About this app"
+        image={require('../assets/masthead.png')}
+      >
+        <Navbar />
+      </Masthead>
+      <ScrollView
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        mt="-20px"
+        pt="30px"
+        p={4}
+      >
+        <VStack flex={1} space={4}>
+          <Box alignItems="center">
+            <Image
+              source={require('../assets/avatar.jpeg')}
+              borderRadius="full"
+              resizeMode="cover"
+              w={120}
+              h={120}
+              alt="author"
+            />
+          </Box>
+          <Text fontSize="md" w="full">
+            This is example of amazing React-native Task App
+          </Text>
+        </VStack>
+      </ScrollView>
+    </AnimatedColorBox>
+  )
+}
+
+export default AboutScreen
